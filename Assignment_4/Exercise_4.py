9cf3fa346f3edabe3a0993d91ecd9bef4d86a598
Akvile Lukauskaite <akvilelukauskaite17@gmail.com>
2024-01-14 20:25:36 +0100

    changed so that the sign of only the first U column and V row is non-negative

--- Assignment_4/Exercise_4.py
+++ Assignment_4/Exercise_4.py
@@ -17,13 +17,18 @@ A = np.array([
 # Perform Singular Value Decomposition
 U, S, Vt = np.linalg.svd(A)
 
+# Ensure the first column of U is non-negative
+U[:, 0] *= np.sign(U[0, 0])
+
+# Ensure the first row of Vt is non-negative
+Vt[0, :] *= np.sign(Vt[0, 0])
+
 # Extract the first two columns of U and Vt
 U1_2 = U[:, :2]
 V1_2 = Vt[:2, :]
 
-# Ensure all elements in U1_2 and V1_2 are non-negative
-U1_2 = np.abs(U1_2)
-V1_2 = np.abs(V1_2)
+print(U1_2)
+print(V1_2)
 
 # Projection of terms onto U1_2
 terms_projection = np.dot(U1_2.T, np.eye(A.shape[0]))
@@ -39,17 +44,18 @@ documents_projection /= np.linalg.norm(documents_projection, axis=0)
 # terms_projection[:, j] represents the projection of the j-th term onto U1_2
 # documents_projection[:, j] represents the projection of the j-th document onto V1_2
 
-
 # Normalize the projections to have norms <= 1
 terms_projection /= np.linalg.norm(terms_projection, axis=0)
+# print(terms_projection)
 documents_projection /= np.linalg.norm(documents_projection, axis=0)
+# print(documents_projection)
 
 # Now, terms_projection and documents_projection contain the desired projections
 # terms_projection[:, j] represents the projection of the j-th term onto U1_2
 # documents_projection[:, j] represents the projection of the j-th document onto V1_2
 
 # Plot terms_projection
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(6, 4))
 plt.scatter(terms_projection[0, :], terms_projection[1, :], marker='o')
 
 # Add labels for each customer
@@ -61,7 +67,7 @@ plt.xlim([0, 1])
 plt.ylim([-1, 1])
 
 # Set axis labels and title
-plt.xlabel('U1_2 Dimension 1')
+plt.xlabel('U1_2 Dimension 1 (Ranking)')
 plt.ylabel('U1_2 Dimension 2')
 plt.title('Projection of Customers onto U1_2')
 
@@ -69,7 +75,7 @@ plt.title('Projection of Customers onto U1_2')
 plt.show()
 
 # Plot documents_projection
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(6, 4))
 plt.scatter(documents_projection[0, :], documents_projection[1, :], marker='o')
 
 # Add labels for each grocery item
@@ -81,7 +87,7 @@ plt.xlim([0, 1])
 plt.ylim([-1, 1])
 
 # Set axis labels and title
-plt.xlabel('V1_2 Dimension 1')
+plt.xlabel('V1_2 Dimension 1 (Ranking)')
 plt.ylabel('V1_2 Dimension 2')
 plt.title('Projection of Grocery Items onto V1_2')
 
